4672e8d9ac3afbe0499ffdab0b653c5f832eb4de
Dave Adams <Dave@DaveAdams.dev>
2026-03-19 16:01:09 -0400

    Added netlify adapter for astrojs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import node from "@astrojs/node";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: netlify(),
   fonts: [{
     provider: fontProviders.google(),
     name: "Gloock",
